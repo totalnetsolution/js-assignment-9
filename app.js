adc3780015079a30c168cf008c2f65ea1e728c98
muhammad-danish <totalnetsolution2@gmail.com>
2024-03-01 04:25:48 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,10 @@
+var math= +prompt("math marks");
+var urdu= +prompt("urdu marks");
+var chem= +prompt("chemistry marks");
+var isl = +prompt("islamiyat marks");
+var eng = +prompt("english marks");
+var obtain = ( math + urdu + chem + isl + eng);
+console.log(obtain)
+var total = "500"
+var  percentage = ( obtain/total*100)
+console.log(percentage)
